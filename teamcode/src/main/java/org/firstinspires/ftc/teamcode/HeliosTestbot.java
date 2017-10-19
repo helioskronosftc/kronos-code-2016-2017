e84136fc89bed93863689e6d7472e007ebb9daf6
Sriram <varadarajan_sriram@intuit.com>
2017-10-19 16:34:36 -0700

    Latest code

---
+++ teamcode/src/main/java/org/firstinspires/ftc/teamcode/HeliosTestbot.java
@@ -0,0 +1,185 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HeliosHardware;
+
+/**
+ * This OpMode uses the common Pushbot hardware class to define the devices on the robot.
+ * All device access is managed through the HardwarePushbot class.
+ * The code is structured as a LinearOpMode
+ *
+ * This particular OpMode executes a POV Game style Teleop for a PushBot
+ * In this mode the left stick moves the robot FWD and back, the Right stick turns left and right.
+ * It raises and lowers the claw using the Gampad Y and A buttons respectively.
+ * It also opens and closes the claws slowly using the left and right Bumper buttons.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Pushbot: Teleop POV", group="Pushbot")
+//@Disabled
+public class HeliosTestbot extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    HeliosHardware robot           = new HeliosHardware();   // Use a Pushbot's hardware
+                                                               // could also use HardwarePushbotMatrix class.
+    double          clawOffset      = 0;                       // Servo mid position
+    final double    CLAW_SPEED      = 0.1 ;                   // sets rate to move servo
+
+    @Override
+    public void runOpMode() {
+        double leftFront;
+        double rightFront;
+        double leftRear;
+        double rightRear;
+        double drive;
+        double turn;
+        double max;
+        double leftServoPos = 0;
+        double rightServoPos = 1.0;
+        int liftChange;
+        int liftPos;
+
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.update();
+        liftPos = robot.lift.getCurrentPosition();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Run wheels in POV mode (note: The joystick goes negative when pushed forwards, so negate it)
+            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
+            // This way it's also easy to just drive straight, or just turn.
+            //drive = -gamepad1.left_stick_y;
+            //turn  =  gamepad1.right_stick_x;
+
+
+            // Combine drive and turn for blended motion.
+            //left  = drive + turn;
+            //right = drive - turn;
+
+            // Normalize the values so neither exceed +/- 1.0
+            /*max = Math.max(Math.abs(left), Math.abs(right));
+            if (max > 1.0)
+            {
+                left /= max;
+                right /= max;
+            }*/
+            liftPos = robot.lift.getCurrentPosition();
+            liftChange = liftPos + (int)(gamepad2.left_stick_y*100);
+            leftFront = gamepad1.left_stick_y - gamepad1.left_stick_x - (gamepad1.right_stick_x * 0.7f);
+            rightFront = gamepad1.left_stick_y + gamepad1.left_stick_x + (gamepad1.right_stick_x * 0.7f);
+            leftRear = gamepad1.left_stick_y + gamepad1.left_stick_x - (gamepad1.right_stick_x * 0.7f);
+            rightRear = gamepad1.left_stick_y - gamepad1.left_stick_x + (gamepad1.right_stick_x * 0.7f);
+
+            max = Math.max(Math.abs(leftFront), Math.abs(rightFront));
+            if (max > 1.0)
+            {
+                leftFront /= max;
+                rightFront /= max;
+            }
+            max = Math.max(Math.abs(leftRear), Math.abs(rightRear));
+            if (max > 1.0)
+            {
+                leftRear /= max;
+                rightRear /= max;
+            }
+            if (gamepad1.right_trigger == 0)
+            {
+                leftFront *= 0.5;
+                rightFront *= 0.5;
+                leftRear *= 0.5;
+                rightRear *= 0.5;
+            }
+
+            /*leftFront = Range.clip(FL_power_raw, -1, 1);
+            rightFront = Range.clip(FR_power_raw, -1, 1);
+            leftRear = Range.clip(RL_power_raw, -1, 1);
+            rightRear = Range.clip(RR_power_raw, -1, 1);*/
+
+            // Output the safe vales to the motor drives.
+            robot.leftDriveFront.setPower(leftFront);
+            robot.rightDriveFront.setPower(rightFront);
+            robot.leftDriveRear.setPower(leftRear);
+            robot.rightDriveRear.setPower(rightRear);
+            if (liftPos+liftChange > 0) {
+                robot.lift.setTargetPosition(0);
+            } else if (liftPos+liftChange < -2000) {
+                robot.lift.setTargetPosition(-2000);
+            } else {
+                robot.lift.setTargetPosition(liftPos+liftChange);
+            }
+            if (gamepad2.y) {
+                leftServoPos = 0.25;
+                rightServoPos = 0.75;
+            }
+            if (gamepad2.b) {
+                leftServoPos = 0.75;
+                rightServoPos = 0.25;
+            }
+            if (gamepad2.a) {
+                robot.lift.setTargetPosition(-1000);
+                while (robot.lift.getCurrentPosition() < -998 || robot.lift.getCurrentPosition() > -1002) {}
+                leftServoPos = 0;
+                rightServoPos = 1.0;
+            }
+
+            // Move both servos to new position.  Assume servos are mirror image of each other.
+            robot.leftClaw.setPosition(leftServoPos);
+            robot.rightClaw.setPosition(rightServoPos);
+
+            // Send telemetry message to signify robot running;
+            telemetry.addData("claw",  "Offset = %.2f", clawOffset);
+            telemetry.addData("left-front",  "%.2f", leftFront);
+            telemetry.addData("right-front", "%.2f", rightFront);
+            telemetry.addData("left-rear",  "%.2f", leftRear);
+            telemetry.addData("right-rear", "%.2f", rightRear);
+            telemetry.addData("right-trigger", "%.2f", gamepad1.right_trigger);
+            telemetry.update();
+
+            // Pace this loop so jaw action is reasonable speed.
+            sleep(50);
+        }
+    }
+}
---
+++ teamcode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -0,0 +1,184 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
+/**
+ * This file illustrates the concept of driving a path based on time.
+ * It uses the common Pushbot hardware class to define the drive on the robot.
+ * The code is structured as a LinearOpMode
+ *
+ * The code assumes that you do NOT have encoders on the wheels,
+ *   otherwise you would use: PushbotAutoDriveByEncoder;
+ *
+ *   The desired path in this example is:
+ *   - Drive forward for 3 seconds
+ *   - Spin right for 1.3 seconds
+ *   - Drive Backwards for 1 Second
+ *   - Stop and close the claw.
+ *
+ *  The code is written in a simple form with no optimizations.
+ *  However, there are several ways that this type of sequence could be streamlined,
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+/*
+@Autonomous(name="Pushbot: Auto Drive By Time", group="Pushbot")
+@Disabled
+public class TestAuto extends LinearOpMode {
+
+    /* Declare OpMode members. */ /*
+    HardwarePushbot         robot   = new HardwarePushbot();   // Use a Pushbot's hardware
+    private ElapsedTime     runtime = new ElapsedTime();
+
+    static final double COUNTS_PER_MOTOR_REV = 1080; //1440 ;    // eg: TETRIX Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double DRIVE_SPEED = 0.6;
+    static final double     TURN_SPEED    = 0.5;
+
+    @Override
+    public void runOpMode() {
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */ /*
+        robot.init(hardwareMap);
+
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        idle();
+
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.addData("Path0", "Starting at %7d :%7d",
+                robot.leftDrive.getCurrentPosition(),
+                robot.rightDrive.getCurrentPosition());
+        telemetry.update();
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+   //     waitForStart();
+
+     //   long debugpause = 500;
+     //   long debugpause2 = 500;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public void forward(double distance, double speed, double offset) {
+            offset = offset / 2;
+            double leftDist = (-distance);
+            double rightDist = (-distance);
+            double time = (distance * (250 * speed));
+            double Lspeed = speed - offset;
+            double Rspeed = speed + offset;
+           // encoderDrive(Lspeed, Rspeed, leftDist, rightDist, time);
+        }
+
+        public void backward(double distance, double speed, double offset) {
+            offset = offset / 2;
+            double leftDist = (distance);
+            double rightDist = (distance);
+            double time = (distance * (250 * speed));
+            double Lspeed = speed - offset;
+            double Rspeed = speed + offset;
+          //  encoderDrive(Lspeed, Rspeed, leftDist, rightDist, time);
+        }
+
+/*
+        // Step through each leg of the path, ensuring that the Auto mode has not been stopped along the way
+
+        // Step 1:  Drive forward for 3 seconds
+        robot.leftDrive.setPower(FORWARD_SPEED);
+        robot.rightDrive.setPower(FORWARD_SPEED);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 3.0)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Step 2:  Spin right for 1.3 seconds
+        robot.leftDrive.setPower(TURN_SPEED);
+        robot.rightDrive.setPower(-TURN_SPEED);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.3)) {
+            telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Step 3:  Drive Backwards for 1 Second
+        robot.leftDrive.setPower(-FORWARD_SPEED);
+        robot.rightDrive.setPower(-FORWARD_SPEED);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.0)) {
+            telemetry.addData("Path", "Leg 3: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Step 4:  Stop and close the claw.
+        robot.leftDrive.setPower(0);
+        robot.rightDrive.setPower(0);
+        robot.leftClaw.setPosition(1.0);
+        robot.rightClaw.setPosition(0.0);
+        */
+
+      //  telemetry.addData("Path", "Complete");
+      //  telemetry.update();
+      //  sleep(1000);
+   // }
+//}
